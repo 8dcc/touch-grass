29097dff9fda1ad358ef72a061e33c0c0f32129e
8dcc <8dcc.git@gmail.com>
2022-12-01 15:47:18 +0100

    Move string arrays to separate file, add macros and make funcs inline

--- src/main.c
+++ src/main.c
@@ -1,48 +1,38 @@
-#include <stdio.h>      // For printing
-#include <string.h>     // For checking the arguments
-#include <stdlib.h>     // For rand()
-#include <time.h>       // For the srand() seed
+#include <stdio.h>     // For printing
+#include <string.h>    // For checking the arguments
+#include <stdlib.h>    // For rand()
+#include <time.h>      // For the srand() seed
 
 #include "colors.h"
+#include "strings.h"
 
-#define STR_NUM 4       // Number of strings for the arrays
+#define length(arr) (sizeof(arr) / sizeof(arr[0]))
+#define max(a, b)   (a > b ? a : b)
 
-static void print_simple(const int str_id);
-static void print_color(const int str_id);
+static inline void print_simple(const int idx) {
+    printf("%s\n", str_arr[idx]);
+}
+
+static inline void print_color(const int idx) {
+    printf("%s\n", str_arr_color[idx]);
+}
 
 int main(int argc, char** argv) {
-    // Check if there are arguments and -c is the first one, then save that in const
-    const int using_color = (argc > 1 && strcmp("-c", argv[1]) == 0) ? 1 : 0;
+    // Check if there are arguments and -c is the first one
+    const int using_color = (argc > 1 && strcmp("-c", argv[1]) == 0);
+
+    const int str_ammount = max(length(str_arr), length(str_arr_color));
 
     // Get a random id for the string.
-    // TODO: Seed changes every sec
+    // TODO: Seed changes every sec, make it even more random.
     srand(time(NULL));
-    const int random_id = rand() % STR_NUM;
+    const int random_id = rand() % str_ammount;
 
-    if (using_color) print_color(random_id);
-    else             print_simple(random_id);
+    if (using_color)
+        print_color(random_id);
+    else
+        print_simple(random_id);
 
     return 0;
 }
 
-static void print_simple(const int str_id) {
-    static const char* str_arr[STR_NUM] = {
-        "Remember to touch some grass.",
-        "Did you have a shower today?",
-        "You should go outside.",
-        "You need the sun light."
-    };
-
-    printf("%s\n", str_arr[str_id]);
-}
-
-static void print_color(const int str_id) {
-    static const char* str_arr[STR_NUM] = {
-        "Remember to touch some " COL_2 "grass" COL_0 ".",
-        "Did you have a " COL_6 "shower" COL_0 " today?",
-        "You should go " COL_3 "outside" COL_0 ".",
-        "You need the " COL_3 "sun" COL_0 " light."
-    };
-
-    printf("%s\n", str_arr[str_id]);
-}
---
+++ src/strings.h
@@ -0,0 +1,15 @@
+// Add here strings for the program. See colors.h for color defines.
+
+const char* str_arr[] = {
+    "Remember to touch some grass.",
+    "Did you have a shower today?",
+    "You should go outside.",
+    "You need the sun light.",
+};
+
+const char* str_arr_color[] = {
+    "Remember to touch some " CGRN "grass" CRST ".",
+    "Did you have a " CCYN "shower" CRST " today?",
+    "You should go " CYLW "outside" CRST ".",
+    "You need the " CYLW "sun" CRST " light.",
+};
