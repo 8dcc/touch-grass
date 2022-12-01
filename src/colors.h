d227996a749201bc7a4d7c434d00177ce4238a12
8dcc <8dcc.git@gmail.com>
2022-12-01 15:46:40 +0100

    Change color defines

--- src/colors.h
+++ src/colors.h
@@ -1,9 +1,9 @@
 
-#define COL_0 "\x1b[0m"       // 0 - Reset
-#define COL_1 "\x1b[31m"      // 1 - Red
-#define COL_2 "\x1b[32m"      // 2 - Green
-#define COL_3 "\x1b[33m"      // 3 - Yellow
-#define COL_4 "\x1b[34m"      // 4 - Blue
-#define COL_5 "\x1b[35m"      // 5 - Magenta
-#define COL_6 "\x1b[36m"      // 6 - Cyan
+#define CRST "\x1b[0m"       // 0 - Reset
+#define CRED "\x1b[31m"      // 1 - Red
+#define CGRN "\x1b[32m"      // 2 - Green
+#define CYLW "\x1b[33m"      // 3 - Yellow
+#define CBLU "\x1b[34m"      // 4 - Blue
+#define CMAG "\x1b[35m"      // 5 - Magenta
+#define CCYN "\x1b[36m"      // 6 - Cyan
 
